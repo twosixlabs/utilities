0b6d7ea11272b0cf1c58b559b39233fc0845a241
Christopher H. Todd <chris.todd@twosixlabs.com>
2018-09-20 11:05:09 -0400

    adding a python2 setup.pyu

---
+++ python/python2/setup.py
@@ -0,0 +1,33 @@
+"""
+    Purpose:
+        setup.py is executed to build the python 2.x project and install
+        the libraries as a pip project. This will allow for the libs
+        to be included in the Dockerfile of projects utilizing these
+        libraries.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='twosix_py2',
+    version='1.0.0',
+    python_requires='<3.0.0',
+    description=(
+        'TwoSixLabs Python 2.x Resuable Libraries'
+    ),
+    url='https://github.com/twosixlabs/utilities/python/python2',
+    author='N/A',
+    author_email='N/A',
+    classifiers=[
+        'Programming Language :: Python :: 2.x',
+    ],
+    keywords=['python', 'libraries'],
+    packages=[
+        'execution_helpers',
+        'logging_helpers',
+    ],
+    install_requires=[
+        'wrapt==1.10.8',
+    ],
+    project_urls={},
+)
