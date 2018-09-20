ef5d433bae16dab8cd99bec8ebc6a59f57ce79a0
Christopher H. Todd <chris.todd@twosixlabs.com>
2018-09-20 11:06:04 -0400

    adding logging_helpers. This is no different than in python3, so the code will be the same for now

---
+++ python/python2/logging_helpers/docker_logging.py
@@ -0,0 +1,34 @@
+"""
+    Purpose:
+        Library for Logging in Docker. This will utilize the logging
+        library to python stout to be captured by the docker log
+        and generate an optional log within the container
+"""
+
+# Python Library Imports
+import logging
+import sys
+
+
+def get_docker_logging(level=logging.INFO):
+    """
+    Purpose:
+        Get Docker-Specific Logger. This will log all events to
+        stdout instead of a log-file so that 'docker logs' will
+        pick up all events.
+    Args:
+        level (log level from logging):
+
+    Examples:
+        >>> from docker_logging import get_logging
+        >>> logging = get_logging()
+    """
+
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=level,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%a, %d %b %Y %H:%M:%S'
+    )
+
+    return logging
