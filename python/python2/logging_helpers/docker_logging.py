808cb4ade5e5425e1785f76f9fd22250de1ffa2d
Christopher H. Todd <chris.todd@twosixlabs.com>
2018-11-02 10:08:18 -0400

    adding params for logging funtion so that more flexibility can be achieved

--- python/python2/logging_helpers/docker_logging.py
+++ python/python2/logging_helpers/docker_logging.py
@@ -10,25 +10,31 @@ import logging
 import sys
 
 
-def get_docker_logging(level=logging.INFO):
+def get_docker_logging(
+    log_level=logging.INFO,
+    log_msg_fmt='%(asctime)s %(levelname)s %(message)s'
+    log_date_fmt='%a, %d %b %Y %H:%M:%S'
+):
     """
     Purpose:
         Get Docker-Specific Logger. This will log all events to
         stdout instead of a log-file so that 'docker logs' will
         pick up all events.
     Args:
-        level (log level from logging):
-
+        level (Logging Level Enum): Level of logs to show (DEBUG, INFO, ERROR)
+        log_msg_fmt (String): Message format. Sets standard for all logs
+        log_date_fmt (String): Data format to prefix log (used if asctime)
+            is in the log_msg_fmt variable
     Examples:
         >>> from docker_logging import get_logging
-        >>> logging = get_logging()
+        >>> logging = get_logging(level=logging.DEBUG)
     """
 
     logging.basicConfig(
         stream=sys.stdout,
-        level=level,
-        format='%(asctime)s %(levelname)-8s %(message)s',
-        datefmt='%a, %d %b %Y %H:%M:%S'
+        level=log_level,
+        format=log_msg_fmt,
+        datefmt=log_date_fmt
     )
 
     return logging
